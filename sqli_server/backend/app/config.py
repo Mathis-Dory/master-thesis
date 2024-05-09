8bdcb82c546d040aa7024c9f56a2ebe6094cdd8f
mathisdory <mathisdory@gmail.com>
2024-05-09 12:15:31 +0200

    refactor(backend): add postgres in config

--- sqli_server/backend/app/config.py
+++ sqli_server/backend/app/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
-    DBMS_IMAGES = ["mysql:latest"]
+    DBMS_IMAGES = ["mysql:latest", "postgres:latest"]
     CHALLENGES_EPISODES = int(os.getenv("CHALLENGES_EPISODES", 10))
     DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
     SEED = os.getenv("SEED")
